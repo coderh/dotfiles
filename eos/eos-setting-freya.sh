84d7b7a5cac98ae0e2bcd03ee69e324bdc20d12f
invkrh <invkrh@gmail.com>
2016-10-02 21:53:51 +0200

    Update freya install

--- eos/eos-setting-freya.sh
+++ eos/eos-setting-freya.sh
@@ -2,14 +2,8 @@
 
 sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily
 sudo add-apt-repository ppa:numix/ppa
-sudo add-apt-repository ppa:moka/stable
-sudo add-apt-repository ppa:yunnxx/elementary
 
 sudo apt-get update
 
 sudo apt-get install elementary-tweaks
-sudo apt-get install elementary-dark-theme elementary-plastico-theme elementary-whit-e-theme elementary-harvey-theme elementary-blue-theme elementary-colors-theme elementary-lion-theme elementary-champagne-theme elementary-milk-theme elementary-emod-icons elementary-elfaenza-icons elementary-nitrux-icons elementary-enumix-utouch-icons elementary-plank-themes
 sudo apt-get install numix-icon-theme-circle
-sudo apt-get install faba-icon-theme moka-icon-theme
-sudo apt-get install faba-mono-icons
-sudo apt-get install elementary-transparent-theme
